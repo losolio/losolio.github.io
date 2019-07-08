b82e0b279cbb046cdd4cef8fa25bb326639f1384
Ole Kristian Losvik <losvik@gmail.com>
2019-07-08 13:30:14 +0200

    asdf

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,39 +1,48 @@
-const path = require('path');
-exports.createPages = ({ boundActionCreators, graphql }) => {
-  const { createPage } = boundActionCreators;
-  const docTemplate = path.resolve(`src/templates/docs-template.js`);
-  return graphql(`
-  {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___title] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            excerpt(pruneLength: 250)
-            html
-            id
-            frontmatter {
-              path
-              title
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+var path = require('path');
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return new Promise((resolve, reject) => {
+    const docsTemplate = path.resolve(`src/templates/docs-template.js`)
+    // Query for markdown nodes to use in creating pages.
+    resolve(
+      graphql(
+        `
+          {
+            allMarkdownRemark(limit: 1000) {
+              edges {
+                node {
+                  html, 
+                  id,
+                  frontmatter {
+                    path,
+                    title
+                  }
+                }
+              }
             }
           }
+        `
+      ).then(result => {
+        if (result.errors) {
+          reject(result.errors)
         }
-      }
-	}
-	`
-	)
-    .then(result => {
-      if (result.errors) {
-        return Promise.reject(result.errors);
-      }
-result.data.allMarkdownRemark.edges
-        .forEach(({ node }) => {
+
+        // Create pages for each markdown file.
+        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+          const path = node.frontmatter.path
           createPage({
-            path: node.frontmatter.path,
-            component: docTemplate,
-            context: {}
-          });
-        });
-    });
-};
+            path,
+            component: docsTemplate,
+            // In your blog post template's graphql query, you can use path
+            // as a GraphQL variable to query for data from the markdown file.
+            context: {
+            },
+          })
+        })
+      })
+    )
+  })
+}
