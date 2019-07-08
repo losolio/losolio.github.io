b60a445dc58a66635718ee646998abb913f62ddc
Ole Kristian Losvik <losvik@gmail.com>
2019-07-08 13:07:36 +0200

    Added utils

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -7,12 +7,12 @@ export const imports = {
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-docs-button" */ 'src/docs/button.mdx'
     ),
-  'src/docs/index.md': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-docs-index" */ 'src/docs/index.md'
-    ),
   'src/docs/getting-started.md': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-docs-getting-started" */ 'src/docs/getting-started.md'
+    ),
+  'src/docs/index.md': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-docs-index" */ 'src/docs/index.md'
     ),
 }
