d4e698b6d2930d466a974e633225e94c9e01d2f7
Ole Kristian Losvik <losvik@gmail.com>
2019-07-11 20:30:18 +0200

    Formatted

--- src/components/doc-navigation/index.js
+++ src/components/doc-navigation/index.js
@@ -1,35 +1,47 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import React from "react";
+import Link from "gatsby-link";
+import { Nav, NavItem, NavLink } from "reactstrap";
 
-const DocNavigation = () => (
-  
-  <div class="pt-5 pl-3">
+export default class DocNavigation extends React.Component {
+  constructor(props) {
+    super(props);
 
-    <header>Navigation</header>
-    <Nav vertical>
-      <NavItem>
-        <NavLink href="/docker">Docker</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="/docker/mssql">Link</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="/toolbox">Toolbox</NavLink>
-      </NavItem>
-    </Nav>
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div class="pt-5 pl-3">
+        <header>Navigation</header>
+        <Nav vertical>
+          <NavItem>
+            <NavLink href="/docker">Docker</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/docker/mssql">Link</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/toolbox">Toolbox</NavLink>
+          </NavItem>
+        </Nav>
 
-    <header class="pt-5">Navigation</header>
-    <Nav vertical>
-    <NavItem>
-        <NavLink href="/web/accessibility">Accessibility</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="/web/flexbox">Flexbox</NavLink>
-      </NavItem>
-    
-      </Nav>
-    </div>
-    
-)
-export default DocNavigation
+        <header class="pt-5">Navigation</header>
+        <Nav vertical>
+          <NavItem>
+            <NavLink href="/web/accessibility">Accessibility</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/web/flexbox">Flexbox</NavLink>
+          </NavItem>
+        </Nav>
+      </div>
+    );
+  }
+}
